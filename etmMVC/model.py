92b5e5dac328644b59170b8bd85ea2a73fd2abdc
Dan Graham <dnlgrhm@gmail.com>
2017-09-07 18:08:26 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -21,7 +21,7 @@ def etm_parse(s):
     if (res.hour, res.minute, res.second, res.microsecond) == (0, 0, 1, 0):
         return res.date()
     else:
-        return res.replace(second=0)
+        return res.replace(second=0, microsecond=0)
 
 
 class DatetimeCacheTable(SmartCacheTable):
