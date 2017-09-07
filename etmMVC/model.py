b58e13369b8fe3192fbffd791ffab41d04750f46
Dan Graham <dnlgrhm@gmail.com>
2017-09-07 18:15:26 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -13,12 +13,12 @@ from tinydb_smartcache import SmartCacheTable
 from dateutil.parser import parse
 from dateutil.tz import (tzlocal, gettz, tzutc)
 
-parse_default = datetime.now().replace(hour=0, minute=1, second=0, 
+parse_default = datetime.now().replace(hour=0, minute=0, second=1, 
 microsecond=0)
 
 def etm_parse(s):
     res = parse(s, default=parse_default)
-    if (res.hour, res.minute, res.second, res.microsecond) == (0, 1, 0, 0):
+    if (res.hour, res.minute, res.second, res.microsecond) == (0, 0, 1, 0):
         return res.date()
     else:
         return res.replace(second=0, microsecond=0)
@@ -104,6 +104,6 @@ if __name__ == '__main__':
     for item in db:
         print(item.eid, item)
 
-    print(etm_parse('fri 2p'))
-    print(etm_parse('fri 12a'))
+    print(et=`=jedi=1, m_parse('=`= (*_*s*_*) =`=jedi=`='fri 2p'))
+    print(etm_parse('fri 12p'))
     print(etm_parse('fri'))
