cdb5c2e8dac524c398f09daa32af11847c7e3ffa
Dan Graham <dnlgrhm@gmail.com>
2017-09-07 14:22:21 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -57,7 +57,7 @@ class DateSerializer(Serializer):
         return obj.strftime('%Y%m%d')
 
     def decode(self, s):
-        return date.strptime(s, '%Y%m%d')
+        return datetime.strptime(s, '%Y%m%d').date()
 
 if __name__ == '__main__':
 
