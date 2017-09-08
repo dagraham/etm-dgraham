9b5ea09e21b516485bac6be385481a5837eed005
Dan Graham <dnlgrhm@gmail.com>
2017-09-08 10:37:09 -0400

    Auto-commit: saved etmMVC/urview.py

---
+++ etmMVC/urview.py
@@ -0,0 +1,31 @@
+def main():
+    term = urwid.Terminal(None)
+
+    mainframe = urwid.LineBox(
+        urwid.Pile([
+            ('weight', 70, term),
+            ('fixed', 1, urwid.Filler(urwid.Edit('focus test edit: '))),
+        ]),
+    )
+
+    def set_title(widget, title):
+        mainframe.set_title(title)
+
+    def quit(*args, **kwargs):
+        raise urwid.ExitMainLoop()
+
+    def handle_key(key):
+        if key in ('q', 'Q'):
+            quit()
+
+    urwid.connect_signal(term, 'title', set_title)
+    urwid.connect_signal(term, 'closed', quit)
+
+    loop = urwid.MainLoop(
+        mainframe,
+        handle_mouse=False,
+        unhandled_input=handle_key)
+
+    term.main_loop = loop
+    loop.run()
+
