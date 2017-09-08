47726d3c2497b073b2690bf219f3709665744c71
Dan Graham <dnlgrhm@gmail.com>
2017-09-08 10:49:09 -0400

    Auto-commit: saved etmMVC/urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -1,34 +1,120 @@
 import urwid
 
+# def main():
+#     term = urwid.Terminal(None)
+
+#     mainframe = urwid.LineBox(
+#         urwid.Pile([
+#             ('weight', 70, term),
+#             ('fixed', 1, urwid.Filler(urwid.Edit('focus test edit: '))),
+#         ]),
+#     )
+
+#     def set_title(widget, title):
+#         mainframe.set_title(title)
+
+#     def quit(*args, **kwargs):
+#         raise urwid.ExitMainLoop()
+
+#     def handle_key(key):
+#         if key in ('q', 'Q'):
+#             quit()
+
+#     urwid.connect_signal(term, 'title', set_title)
+#     urwid.connect_signal(term, 'closed', quit)
+
+#     loop = urwid.MainLoop(
+#         mainframe,
+#         handle_mouse=False,
+#         unhandled_input=handle_key)
+
+#     term.main_loop = loop
+#     loop.run()
+
 def main():
-    term = urwid.Terminal(None)
+    palette =   [
+                ('header', 'dark magenta,bold', 'default'),
+                ('footer', 'black', 'light gray'),
+                ('textentry', 'white,bold', 'dark red'),
+                ('body', 'light gray', 'default'),
+                ('focus', 'black', 'dark cyan', 'standout')
+                ]
 
-    mainframe = urwid.LineBox(
-        urwid.Pile([
-            ('weight', 70, term),
-            ('fixed', 1, urwid.Filler(urwid.Edit('focus test edit: '))),
-        ]),
-    )
+    textentry = urwid.Edit()
+    assert textentry.get_text() == ('', []), textentry.get_text() 
 
-    def set_title(widget, title):
-        mainframe.set_title(title)
+    parser = OptionParser()
+    parser.add_option("-u", "--username")
+    parser.add_option("-p", "--password")
+    (options, args) = parser.parse_args()
 
-    def quit(*args, **kwargs):
-        raise urwid.ExitMainLoop()
+    if options.username and not options.password:
+        print "If you specify a username, you must also specify a password"
+        exit()
 
-    def handle_key(key):
-        if key in ('q', 'Q'):
-            quit()
+    print "Loading..."
 
-    urwid.connect_signal(term, 'title', set_title)
-    urwid.connect_signal(term, 'closed', quit)
+    body = MainWindow()
+    if options.username:
+        print "[Logging in]"
+        if body.login(options.username, options.password):
+            print "[Login Successful]"
+        else:
+            print "[Login Failed]"
+            exit()
 
-    loop = urwid.MainLoop(
-        mainframe,
-        handle_mouse=False,
-        unhandled_input=handle_key)
+    body.refresh()
 
-    term.main_loop = loop
-    loop.run()
+    def edit_handler(keys, raw):
+        """respond to keys while user is editing text""" 
+        if keys in (['enter'],[]):
+            if keys == ['enter']:
+                if textentry.get_text()[0] != '':
+                    # We set the footer twice because the first time we
+                    # want the updated status text (loading...) to show 
+                    # immediately, and the second time as a catch-all
+                    body.frame.set_footer(body.footer)
+                    body.set_subreddit(textentry.edit_text)
+                    textentry.set_edit_text('')
+            # Restore original status footer
+            body.frame.set_footer(body.footer)
+            body.frame.set_focus('body')
+            global main_loop
+            main_loop.input_filter = input_handler
+            return
+        return keys
+
+    def input_handler(keys, raw):
+        """respond to keys not handled by a specific widget"""
+        for key in keys:
+            if key == 's':
+                # Replace status footer wth edit widget
+                textentry.set_caption(('textentry', ' [subreddit?] ("fp" for the front page) :>'))
+                body.frame.set_footer(urwid.Padding(textentry, left=4))
+                body.frame.set_focus('footer')
+                global main_loop
+                main_loop.input_filter = edit_handler
+                return
+            elif key in ('j','k'):
+                direction = 'down' if key == 'j' else 'up'
+                return [direction]
+            elif key in ('n','m'):
+                direction = 'prev' if key == 'n' else 'next'
+                body.switch_page(direction)
+            elif key == 'u':
+                body.refresh()
+            elif key == 'b': # boss mode
+                os.system("man python")
+            elif key == '?': # help mode
+                os.system("less -Ce README.markdown")
+            elif key == 'q': # quit
+                raise urwid.ExitMainLoop()
+            return keys
+
+    # Start ui 
+    global main_loop
+    main_loop = urwid.MainLoop(body.frame, palette, input_filter=input_handler)
+    main_loop.run()
+
 
 main()
