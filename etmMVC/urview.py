14542b2d9fec84c2afa898ffca0bf670368f935c
Dan Graham <dnlgrhm@gmail.com>
2017-09-11 12:51:37 -0400

    Auto-commit: saved etmMVC/urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -240,8 +240,6 @@ def on_ask_change(edit, new_edit_text):
                 at_hsh[part[0]] = '?'
             at_tups.append( (part[0], at_hsh[part[0]], tmp) )
 
-        while 
-
         print('\n', at_tups)
 
         itemtype, summary, end = at_tups.pop(0)
