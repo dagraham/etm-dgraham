b7d557996df287c9dbf4747eacf682338f685f96
Dan Graham <dnlgrhm@gmail.com>
2017-09-11 20:40:16 -0400

    Auto-commit: saved etmMVC/urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -252,7 +252,8 @@ def on_ask_change(edit, new_edit_text):
             else:
                 break
         if itemtype in type_keys:
-            ask.set_caption(('I say', "new {0} pos {1} act {2}\n".format(type_keys[itemtype], pos, act_key)))
+            # ask.set_caption(('I say', "{0} pos {1} act {2}\n".format(type_keys[itemtype], pos, act_key)))
+            ask.set_caption(('I say', "{0} summary?\n  ".format(type_keys[itemtype], pos, act_key)))
             if act_key == itemtype:
                 if act_val:
                     reply.set_text(('I say', "summary (string):\n  {0}".format(act_val)))
