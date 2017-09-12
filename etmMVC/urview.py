7d0225d603b5db6f6f6c2ac17cf6ac4dd43d65ed
Dan Graham <dnlgrhm@gmail.com>
2017-09-12 09:35:35 -0400

    Auto-commit: saved etmMVC/urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -275,7 +275,7 @@ def on_ask_change(edit, new_edit_text):
                 reply.set_text(('say', "@{0} is invalid".format(act_key)))
 
         else:
-            ask.set_caption(('warn', u"Invalid item type '{0}'.\nUse * (event), - (task), # (journal entry), ? (someday entry) or ! (nbox entry)\n".format(itemtype)))
+            reply.set_text(('warn', u"Invalid item type '{0}'.\nUse * (event), - (task), # (journal entry), ? (someday entry) or ! (nbox entry)\n".format(itemtype)))
             summary = "{0}{1}".format(itemtype, summary)
     else:
         reply.set_text(('say', "no tups"))
