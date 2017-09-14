7aa66c49b263bc3ab99a0da8ba1722299f15d8cc
Dan Graham <dnlgrhm@gmail.com>
2017-09-14 15:51:41 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     db.insert({'time delta': timedelta(weeks=1, days=3, hours=7, minutes=15)})
     rr = rrulestr('DTSTART:20170914T105932\nFREQ=MONTHLY;INTERVAL=2;COUNT=10;BYDAY=-1SU,+1SU')
     db.insert({'tr': rr})
-    set = rrule.rruleset()
+    set = rruleset()
     set.rrule(rrule(WEEKLY, count=4, dtstart = datetime(2017, 9, 14, 9, 0)))
     hsh = {'type': '*', 'summary': 'my event', 's':  datetime(2017, 9, 7, 12, 0, 0, tzinfo=gettz('US/Pacific')), 'e': timedelta(hours=1, minutes=15)}
     db.insert(hsh)
