f75d8587ddce91144eecb20e4be247fef40bf252
Dan Graham <dnlgrhm@gmail.com>
2017-09-14 11:30:47 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -120,7 +120,7 @@ class RruleSerializer(Serializer):
     All rrule datetimes are naive. When the rrule object is first created, the value for dtstart will be taken from the value of `s`, which is required and may either be aware, UTC time or naive. If aware, then all the rrule instances will need to be converted to localtime. 
     """
 
-    OBJ_CLASS = rrule  # The class handles rrule objects
+    OBJ_CLASS = dateutil.rrule  # The class handles rrule objects
 
 
     def encode(self, obj):
