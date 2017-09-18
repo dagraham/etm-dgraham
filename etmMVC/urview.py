4e0b016e617cd94de0778cbc82b0b71b6706a12c
Dan Graham <dnlgrhm@gmail.com>
2017-09-18 15:27:34 -0400

    Auto-commit: saved urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -7,7 +7,7 @@ cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
-from etmMVC.container import check_entry
+from container import check_entry
 
 
 # def main():
