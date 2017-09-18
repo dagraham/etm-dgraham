711a8a544500a869c178fc96253ffe76184581cc
Dan Graham <dnlgrhm@gmail.com>
2017-09-18 15:26:54 -0400

    Auto-commit: saved urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -1,4 +1,11 @@
 import urwid
+
+import os, sys, inspect
+ # realpath() will make your script run, even if you symlink it :)
+ cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+ if cmd_folder not in sys.path:
+     sys.path.insert(0, cmd_folder)
+
 from etmMVC.container import check_entry
 
 
