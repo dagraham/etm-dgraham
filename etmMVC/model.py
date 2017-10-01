b6930f20d449920fe7670b6a4755ec0b09840514
Dan Graham <dnlgrhm@gmail.com>
2017-10-01 18:37:26 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -90,36 +90,36 @@ class PendulumDateTimeSerializer(Serializer):
             return pendulum.from_format(s[:-1], '%Y%m%dT%H%M', 'Factory')
 
 
-class DateTimeSerializer(Serializer):
-    """
-    This class handles both aware and naive datetime objects. 
+# class DateTimeSerializer(Serializer):
+#     """
+#     This class handles both aware and naive datetime objects. 
 
-    Encoding: If the datetime object is aware, it is first converted to UTC and then encoded with an 'A' appended to the serialization. Otherwise it is serialized without conversion and an 'N' is appended.
+#     Encoding: If the datetime object is aware, it is first converted to UTC and then encoded with an 'A' appended to the serialization. Otherwise it is serialized without conversion and an 'N' is appended.
 
-    Decoding: If the serialization ends with 'A', the datetime object is treated as UTC and then converted to localtime. Otherwise, the datetime object is treated as localtime and no conversion is necessary.
+#     Decoding: If the serialization ends with 'A', the datetime object is treated as UTC and then converted to localtime. Otherwise, the datetime object is treated as localtime and no conversion is necessary.
 
-    This serialization discards both seconds and microseconds but preserves hours and minutes.
-    """
+#     This serialization discards both seconds and microseconds but preserves hours and minutes.
+#     """
 
-    OBJ_CLASS = datetime
+#     OBJ_CLASS = datetime
 
-    def encode(self, obj):
-        """
-        Serialize naive datetimes objects without conversion but with 'N' for 'Naive' appended. Convert aware datetime objects to UTC and then serialize them with 'A' for 'Aware' appended.
-        """
-        if obj.tzinfo is None:
-            return obj.strftime('%Y%m%dT%H%MN')
-        else:
-            return obj.astimezone(tzutc()).strftime('%Y%m%dT%H%MA')
+#     def encode(self, obj):
+#         """
+#         Serialize naive datetimes objects without conversion but with 'N' for 'Naive' appended. Convert aware datetime objects to UTC and then serialize them with 'A' for 'Aware' appended.
+#         """
+#         if obj.tzinfo is None:
+#             return obj.strftime('%Y%m%dT%H%MN')
+#         else:
+#             return obj.astimezone(tzutc()).strftime('%Y%m%dT%H%MA')
 
-    def decode(self, s):
-        """
-        Return the serialization as a datetime object. If the serializaton ends with 'A',  first converting to localtime and returning an aware datetime object. If the serialization ends with 'N', returning without conversion as a naive datetime object. 
-        """
-        if s[-1] == 'A':
-            return datetime.strptime(s[:-1], '%Y%m%dT%H%M').replace(tzinfo=tzutc()).astimezone(tzlocal())
-        else:
-            return datetime.strptime(s[:-1], '%Y%m%dT%H%M')
+#     def decode(self, s):
+#         """
+#         Return the serialization as a datetime object. If the serializaton ends with 'A',  first converting to localtime and returning an aware datetime object. If the serialization ends with 'N', returning without conversion as a naive datetime object. 
+#         """
+#         if s[-1] == 'A':
+#             return datetime.strptime(s[:-1], '%Y%m%dT%H%M').replace(tzinfo=tzutc()).astimezone(tzlocal())
+#         else:
+#             return datetime.strptime(s[:-1], '%Y%m%dT%H%M')
 
 
 class DateSerializer(Serializer):
