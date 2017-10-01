c9f0354900217ccabd6b69e15cafd4649952f566
Dan Graham <dnlgrhm@gmail.com>
2017-10-01 16:31:23 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -340,6 +340,10 @@ if __name__ == '__main__':
 
     db = TinyDB('db.json', storage=serialization)
     db.purge()
+
+    db.insert({'eastern pendulum': pendulum.Pendulum(2017, 9, 7, 12, 0, 0, tzinfo=gettz('US/Eastern')) })
+    db.insert({'local dt': pendulum.Pendulum(2017, 9, 7, 12, 0, 0, tzinfo=tzlocal()) })
+
     db.insert({'list of dt': [datetime(2017, 9, 7, 12, 0, 0), datetime(2017, 9, 7, 12, 0, 0, tzinfo=gettz('US/Pacific'))]})
     db.insert({'eastern dt': datetime(2017, 9, 7, 12, 0, 0, tzinfo=gettz('US/Eastern')) })
     db.insert({'local dt': datetime(2017, 9, 7, 12, 0, 0, tzinfo=tzlocal()) })
