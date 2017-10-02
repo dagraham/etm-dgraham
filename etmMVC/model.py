39f00baf9748fd563beb29926936fddac9f2436e
Dan Graham <dnlgrhm@gmail.com>
2017-10-02 11:06:44 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -244,7 +244,7 @@ def parse_period(s):
     >>> 3*60*60+5*60
     11100
     >>> parse_period("2d3h5m")[1]
-    datetime.timedelta(2, 11100)
+    <Interval [2 days 3 hours 5 minutes]>
     >>> datetime(2015, 10, 15, 9, 0) + parse_period("-25m")[1]
     datetime.datetime(2015, 10, 15, 8, 35)
     >>> datetime(2015, 10, 15, 9, 0) + parse_period("1d")[1]
