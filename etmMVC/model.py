08fb90b1c4a1a012c69db53fc6f27ecfd0deea6d
Dan Graham <dnlgrhm@gmail.com>
2017-10-02 13:35:28 -0400

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -194,7 +194,7 @@ def parse_datetime(s, tz=None):
     <Pendulum [2015-10-15T14:00:00-04:00]>
     >>> dt = parse_datetime("2015-10-15 0h")
     >>> dt
-    <Pendulum [2015-10-15T00:00:00-04:00]>
+    <Date [2015-10-15]>
     >>> dt = parse_datetime("2015-10-15 00:00:01")
     >>> dt
     <Pendulum [2015-10-15T00:00:00-04:00]>
