3e8958c0e967092f1f92bce19df813cb54324fe5
Dan Graham <dnlgrhm@gmail.com>
2017-10-03 16:07:26 -0400

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -300,7 +300,7 @@ def check_entry(s, cursor_pos):
 
     else:
         ask = ('warn', u"invalid item type character: '{0}'\n".format(itemtype))
-    reply = ('say', reply[1] + "\nat_entry: {}\n".format(at_entry))
+    reply = ('say', reply[1] + "\n\nat_entry: {}\n".format(at_entry))
 
     return ask, reply
 
