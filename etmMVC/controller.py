e2a89e10db9f4c7e1b0ecc86b7f3ccb61995d694
Dan Graham <dnlgrhm@gmail.com>
2017-10-05 10:51:47 -0400

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -141,13 +141,13 @@ def deal_with_s(at_hsh = {}):
         bot = "starting: {}".format(obj.format("ddd MMM D"))
         bot += '\nWithout a time, this schedules an all-day, floating item for the specified date in whatever happens to be the local timezone.'
     elif ok == 'naive':
-        bot = "starting: {}".format(obj.format("ddd MMM D h:mmA"))
+        bot = "starting: {}".format(obj.format("ddd MMM D h:mm A"))
         bot += "\nThe datetime entry for @s will be interpreted as a naive datetime in whatever happens to be the local timezone."
     elif ok == 'aware':
-        bot = "starting: {}".format(obj.format("ddd MMM D h:mma z"))
+        bot = "starting: {}".format(obj.format("ddd MMM D h:mm A z"))
         bot += "\nThe datetime entry for @s will be interpreted as an aware datetime in the specified timezone."
     else:
-        bot = "starting: {}".format(obj.format("ddd MMM D h:mma z"))
+        bot = "starting: {}".format(obj.format("ddd MMM D h:mmA z"))
         bot += "\nThe datetime entry for @s will be interpreted as an aware datetime in the current local timezone. Append a comma and then 'float' to make the datetime floating (naive) or a specific timezone, e.g., 'US/Pacific', to use that timezone instead of the local one."
 
     return top, bot, obj
