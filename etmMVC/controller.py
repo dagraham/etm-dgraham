8ee4b22ce70907fb0f0ffe0420712964c5dc010d
Dan Graham <dnlgrhm@gmail.com>
2017-10-05 10:06:17 -0400

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -67,9 +67,6 @@ amp_keys = {
         'u': "until: datetime",
         'w': "weekday: list from SU, MO, ..., SA",
     },
-    's': {
-        'z': "timezone (string)",
-        },
     'j': {
         'a': "alert: timeperiod: command, args*",
         'b': "beginby: integer number of days",
@@ -301,6 +298,7 @@ def check_entry(s, cursor_pos):
     else:
         ask = ('warn', u"invalid item type character: '{0}'\n".format(itemtype))
 
+    # for testing and debugging:1
     reply = ('say', reply[1] + "\nat_entry {0} {1}: {2}; pos {3}\n{4}\n{5}".format(at_entry, act_key, act_val, cursor_pos, at_tups, at_parts))
 
     return ask, reply
