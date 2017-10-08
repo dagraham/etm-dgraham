31bd4b881701d247fa7b8122198e546e124ab1c9
Dan Graham <dnlgrhm@gmail.com>
2017-10-08 19:53:28 -0400

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -128,10 +128,9 @@ def check_requires(key, hsh):
     Check that hsh has the prerequisite entries for key.
     """
     if key in requires and requires[key] not in hsh:
-        return False, ('warn', "@{0} is required for @{1} ".format(requires[key], key))
+        return False, ('warn', "Please enter @{0} first since it is required for @{1} ".format(requires[key], key))
     else:
         return True, ('say', '')
-
 
 
 type_prompt = u"type character for new item:\n"
