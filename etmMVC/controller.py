e2613aa522360cdaa1c9cb8de2b2c31bde92b75e
Dan Graham <dnlgrhm@gmail.com>
2017-10-11 14:14:37 -0400

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -190,7 +190,7 @@ def deal_with_e(at_hsh={}):
     if not ok:
         return top, "considering: '{}'".format(s), None
     item_hsh['e'] = obj
-    bot = "extending from {0} until {1}".format(item_hsh['s'].format("ddd MMM D h:mmA"), (item_hsh['s'] + item_hsh['e']).format("ddd MMM D h:mmA"))
+    bot = "ending {0} after the starting time".format(item_hsh['e'].in_words())
     # bot += "\n\n{}".format(str(at_hsh))
     return top, bot, obj
 
