5f048dcf215720f0fe450f922238eb9941fe0d06
Dan Graham <dnlgrhm@gmail.com>
2017-11-09 15:45:52 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -319,7 +319,7 @@ def check_entry(s, cursor_pos):
                     if act_key in deal_with:
                         top, bot, obj = deal_with[act_key](hsh)
                         ask = ('say', top)
-                        reply = ('say', bot + '\n')
+                        reply = ('say', "{}\n".format(bot))
 
                     elif act_val:
                         ask = ('say', "{0}: {1}\n".format(at_keys[act_key], act_val))
