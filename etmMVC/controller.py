b76e9ab0c6f209b1f1775d429ada64390a055e9c
Dan Graham <dnlgrhm@gmail.com>
2017-11-09 13:41:30 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -317,7 +317,7 @@ def check_entry(s, cursor_pos):
                     ask = ('say', '{0}\n'.format(at_keys[act_key]))
                     reply = res
 
-                if act_key in allowed[itemtype] and ok:
+                elif act_key in allowed[itemtype]:
                     if act_key in deal_with:
                         top, bot, obj = deal_with[act_key](hsh)
                         ask = ('say', top)
