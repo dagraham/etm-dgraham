42c02d1ba1b666e0cb4d9dcc7d02702cc59b7d83
Dan Graham <dnlgrhm@gmail.com>
2017-12-12 15:07:30 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -318,7 +318,7 @@ def deal_with_r(at_hsh={}):
     bot = "repetition rule:\n{}".format(res)
     if testing:
         out = rrulestr(res, dtstart=item_hsh['s'])
-        lst = [x.astimezone().format("YYYYMMDD[T]HHmm[00]") for x in list(out)]
+        lst = [x.astimezone().format("YYYYMMDD[T]HHmm z") for x in list(out)]
         outstr = "\n".join(lst[:3]) 
         bot += "\n" + outstr
 
