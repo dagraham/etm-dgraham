9c72e7a8449b69adc5f615f5e9f845e8b547a40f
Dan Graham <dnlgrhm@gmail.com>
2017-12-12 14:48:44 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -205,7 +205,7 @@ def deal_with_s(at_hsh = {}):
         bot = "starting: {}".format(obj.in_tz('local').format("ddd MMM D h:mmA z"))
         bot += "\nThe datetime entry for @s will be interpreted as an aware datetime in the specified timezone."
     else:
-        bot = "starting: {}".format(obj.format("ddd MMM D h:mmA z"))
+        bot = "starting: {}".format(obj.in_tz('local').format("ddd MMM D h:mmA z"))
         bot += "\nThe datetime entry for @s will be interpreted as an aware datetime in the current local timezone. Append a comma and then 'float' to make the datetime floating (naive) or a specific timezone, e.g., 'US/Pacific', to use that timezone."
 
     return top, bot, obj
