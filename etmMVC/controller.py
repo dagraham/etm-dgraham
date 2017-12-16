c9b0bfe3d08aa5a4f019e9d7805f73d7cbc8efd7
Dan Graham <dnlgrhm@gmail.com>
2017-12-16 10:40:08 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -95,7 +95,7 @@ task_methods = 'fjp'
 
 # events
 required['*'] = 's'
-allowed['*'] = [x for x in undated_methods + datetime_methods].sort()
+allowed['*'] = undated_methods + datetime_methods
 
 
 # tasks
