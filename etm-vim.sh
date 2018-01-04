6f591cd1ef68f2338696343aacedc940f7c5823d
Dan Graham <dnlgrhm@gmail.com>
2018-01-04 12:03:47 -0500

    Auto-commit: saved etm-vim.sh

--- etm-vim.sh
+++ etm-vim.sh
@@ -1,1 +1,1 @@
-/Applications/MacVim.app/Contents/MacOS/Vim -g  -p etmMVC/model.py etmMVC/urview.py etmMVC/controller.py notes.md tables.md
+/Applications/MacVim.app/Contents/MacOS/Vim -g  -p etmMVC/model.py etmMVC/urview.py etmMVC/controller.py notes.md new.md
