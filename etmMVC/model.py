48a9f9a6053682643ee4ec9a60a8a04c1c11dc19
Dan Graham <dnlgrhm@gmail.com>
2018-01-05 11:41:48 -0500

    Auto-commit: saved etmMVC/model.py

--- etmMVC/model.py
+++ etmMVC/model.py
@@ -299,8 +299,8 @@ def wrap(txt, indent=5, width=shutil.get_terminal_size()[0]):
 def set_summary(s, dt=pendulum.Pendulum.now()):
     """
     Replace the anniversary string in s with the ordinal represenation of the number of years between the anniversary string and dt.
-    >>> ok, dt = parse_datetime('1/5/2018 2p')
-    >>> set_summary('!1944! birthday', dt=dt)
+    >>> ok, s = parse_datetime('1/5/2018 2p')
+    >>> set_summary('!1944! birthday', dt=s)
     '73rd birthday'
     >>> set_summary('!1978! anniversary', pendulum.Pendulum(2017, 11, 19))
     '39th anniversary'
