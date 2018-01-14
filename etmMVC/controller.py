1f796f8c878af74f445d53f78e8600956a87c26b
Dan Graham <dnlgrhm@gmail.com>
2018-01-14 12:42:11 -0500

    Auto-commit: saved etmMVC/controller.py

--- etmMVC/controller.py
+++ etmMVC/controller.py
@@ -331,7 +331,7 @@ def deal_with_j(at_hsh={}):
     top = "job?\n"
     bot = "{}".format(at_keys['j'])
     lofh = at_hsh.get('j', [])
-    ok, res = jobs(lofh, dated)
+    ok, res = jobs(lofh)
     if ok:
         item_hsh['jobs'] = res
         bot = "jobs:\n    {}\n".format(res)
