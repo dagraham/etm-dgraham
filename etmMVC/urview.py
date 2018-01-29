a8e76647d27352a1e1c3c428808bd5e1934856d3
Dan Graham <dnlgrhm@gmail.com>
2018-01-29 09:48:03 -0500

    Auto-commit: saved etmMVC/urview.py

--- etmMVC/urview.py
+++ etmMVC/urview.py
@@ -174,7 +174,7 @@ buttons = urwid.Padding(urwid.GridFlow(
     [save_button, exit_button], 8, 3, 1, 'left'),
      left=4, right=3, min_width=10)
 dates = urwid.Padding(urwid.GridFlow(
-    [date1, date2], 6, 2, 1, 'left'),
+    [date1, date2], 6, 1, 1, 'left'),
      left=4, right=3, min_width=10)
 div = urwid.Divider('-')
 # pile = urwid.Pile([ask, div, reply, div, save_button, exit_button])
