5508a32f4de5d03ac401d6a5a99c5910b16973f0
Dan Graham <dnlgrhm@gmail.com>
2018-02-13 14:14:09 -0500

    Auto-commit: saved /Users/dag/etm-mvc/viewtest.py

--- viewtest.py
+++ viewtest.py
@@ -1,33 +1,6 @@
 import urwid
 import pendulum
 
-
-# This function handles input not handled by widgets.
-# It's passed into the MainLoop constructor at the bottom.
-
-def unhandled_input(key):
-  if key == 'f8':
-    raise urwid.ExitMainLoop()
-
-# A class that is used to create new views, which are
-# two text widgets, piled, and made into a box widget with
-# urwid filler
-class MainView(object):
-    def __init__(self,title_text,body_text):
-      self.title_text = title_text
-      self.body_text = body_text
-
-    def build(self):
-      title = urwid.Text(self.title_text)
-      body = urwid.Text(self.body_text)
-      body = urwid.Pile([title,body])
-      fill = urwid.Filler(body)
-      return fill
-
-# An iterator consisting of 3 instantiated MainView objects.
-# When a user presses Enter, since that particular key sequence
-# isn't handled by a widget, it gets passed into unhandled_input.
-
 logo = [
     "                                ",
     " ███████╗ ████████╗ ███╗   ███╗ ",
@@ -38,7 +11,6 @@ logo = [
     " ╚══════╝    ╚═╝    ╚═╝     ╚═╝ ",
     "                                ",
     ]
-
 
 menu = [
     "--- VIEWS ----------------------------------------- ",
@@ -55,98 +27,94 @@ menu = [
     "J: jump to date  C: copy view      F3: yearly       ",
     "A: alerts        P: preferences    F8: quit         ",
     "                                                    ",
-    ]
+]
 
 menu_text = [
     "The key bindings for the various commands are listed above. E.g., press 'a' to open agenda view. In any of the views, 'Enter' toggles the expansion of the selected node or item. In any of the dated views, 'Shift Left' and 'Shift Right' change the period displayed and 'Space' changes the display to the current date."
     ]
 
 
-# views = [ MainView(title_text=logo,body_text=menu),
-#           MainView(title_text='Page Two',body_text='consectetur adipiscing elit.'),
-#           MainView(title_text='Page Three',body_text='Etiam id hendrerit neque.')
-#         ]
-
-# initial_view = views[0].build()
-# loop = urwid.MainLoop(initial_view,unhandled_input=unhandled_input)
-# # loop.run()
+def unhandled_input(key):
+    """
+    This function handles input not handled by widgets. It's passed into the MainLoop constructor at the bottom.
+    """
+    if key == 'f8':
+        raise urwid.ExitMainLoop()
 
 
 class BaseView:
     """
-    Base view class to be inherited by others. Provides screen setup and palette with methods set_header, set_footer and set_body.
+    Base view class to be inherited by others. Provides methods set_header, set_footer and set_body.
     """
+
+    palette = [
+        # ('foot', 'yellow', 'dark gray', 'standout', '', 'h8'),
+        ('foot', 'yellow', 'dark gray', 'standout', '', 'h8'),
+        ('title', 'yellow', 'dark gray', 'standout', '', 'h8'),
+        ('key', 'white', 'dark gray','standout'),
+        ('body', 'black', 'white', 'default'),
+        ('focus', 'default', 'light gray', '', '', '#adf'),
+        ('edit', 'black', 'dark green', 'standout', 'bold', '#ad0'), # foot in edit mode
+        ('details', 'dark blue', 'white', 'bold'),    # item details
+        ('errors', 'dark red', 'white', 'bold'),      # job pastdue
+        (None, 'black', 'white',''),
+        ('logo', 'dark blue', 'white', ''),     # logo
+        ('path', 'black', 'default', ''),     # path branch
+        ('buttn','black','light gray', '', '', 'g89'),
+        ('buttnf','white','dark blue','bold', '', '#adf'),
+        ('timer', 'black', 'yellow', 'default'),       # path branch
+        ('ib', 'light red', 'default', 'default'),     # inbox
+        ('oc', 'light magenta', 'default', 'default'), # occasion
+        ('ev', 'dark green', 'default', 'standout', '#080', ''),    # event
+        ('rm', 'dark green', 'default', 'default', '#080', ''),   # reminder
+        ('td', 'dark red', 'default', 'default'),      # task pastdue
+        ('jd', 'dark red', 'default', 'default'),      # job pastdue
+        ('dd', 'dark red', 'default', 'default'),      # delegated pastdue
+        ('jw', 'light red', 'default', 'default'),     # job scheduled and waiting
+        ('ts', 'dark blue', 'default', 'default', '#00f', ''),     # task or task group scheduled
+        ('js', 'dark blue', 'default', 'default', '#00f', ''),     # job scheduled and available
+        ('ds', 'dark blue', 'default', 'default', '#00f', ''),     # delegated scheduled
+        ('jp', 'light blue', 'default', 'default'),    # job scheduled with unfinished prereqs
+        ('tu', 'light blue', 'default', 'default'),    # task or task group unscheduled
+        ('ja', 'light blue', 'default', 'default'),    # job unscheduled
+        ('du', 'light blue', 'default', 'default'),    # delegated unscheduled
+        ('jb', 'light cyan', 'default', 'default'),    # job unscheduled with unfinished prereqs
+        ('by', 'dark magenta', 'default', 'default'),  # beginby"
+        ('ac', 'dark cyan', 'default', 'default'),     # action
+        ('ns', 'brown', 'default', 'default'),         # note scheduled
+        ('nu', 'brown', 'default', 'default'),         # note unscheduled
+        ('so', 'light blue', 'default', 'default'),    # someday
+        ('fn', 'dark gray', 'default', 'default'),     # finished task or job
+        ('df', 'brown', 'default', 'default'),     # default
+        ('dl', 'light gray', 'default', 'default'),    # deleted
+        ('co', 'light gray', 'default', 'default'),    # commented out
+        ]
+
+    screen = urwid.raw_display.Screen()
+    # placeholder = urwid.SolidFill()
+    # Frame provices body, header and footer for each view.
+    view = urwid.Frame(None, None, None)
+    loop = urwid.MainLoop(
+        urwid.SolidFill(),
+        screen=screen,
+        unhandled_input=unhandled_input,
+    )
+
+
+    today = None
+    this_week = None
 
     def __init__(self, etmdir=None):
         if etmdir is None:
             etmdir = "/Users/dag/etm-qml/test/data"
+        # self.view = view
+        # self.loop = loop
+        BaseView.screen.set_mouse_tracking()
+        BaseView.screen.tty_signal_keys('undefined', 'undefined', 'undefined', 'undefined', 'undefined')
+        BaseView.screen.set_terminal_properties(256)
+        BaseView.screen.register_palette(BaseView.palette)
+        BaseView.loop.widget = BaseView.view
 
-        self.palette = [
-            # ('foot', 'yellow', 'dark gray', 'standout', '', 'h8'),
-            ('foot', 'yellow', 'dark gray', 'standout', '', 'h8'),
-            ('title', 'yellow', 'dark gray', 'standout', '', 'h8'),
-            ('key', 'white', 'dark gray','standout'),
-            ('body', 'black', 'white', 'default'),
-            ('focus', 'default', 'light gray', '', '', '#adf'),
-            ('edit', 'black', 'dark green', 'standout', 'bold', '#ad0'),       # foot in edit mode
-            ('details', 'dark blue', 'white', 'bold'),    # item details
-            ('errors', 'dark red', 'white', 'bold'),      # job pastdue
-            (None, 'black', 'white',''),
-            ('logo', 'dark blue', 'white', ''),     # logo
-            ('path', 'black', 'default', ''),     # path branch
-            ('buttn','black','light gray', '', '', 'g89'),
-            ('buttnf','white','dark blue','bold', '', '#adf'),
-            ('timer', 'black', 'yellow', 'default'),       # path branch
-            ('ib', 'light red', 'default', 'default'),     # inbox
-            ('oc', 'light magenta', 'default', 'default'), # occasion
-            ('ev', 'dark green', 'default', 'standout', '#080', ''),    # event
-            ('rm', 'dark green', 'default', 'default', '#080', ''),   # reminder
-            ('td', 'dark red', 'default', 'default'),      # task pastdue
-            ('jd', 'dark red', 'default', 'default'),      # job pastdue
-            ('dd', 'dark red', 'default', 'default'),      # delegated pastdue
-            ('jw', 'light red', 'default', 'default'),     # job scheduled and waiting
-            ('ts', 'dark blue', 'default', 'default', '#00f', ''),     # task or task group scheduled
-            ('js', 'dark blue', 'default', 'default', '#00f', ''),     # job scheduled and available
-            ('ds', 'dark blue', 'default', 'default', '#00f', ''),     # delegated scheduled
-            ('jp', 'light blue', 'default', 'default'),    # job scheduled with unfinished prereqs
-            ('tu', 'light blue', 'default', 'default'),    # task or task group unscheduled
-            ('ja', 'light blue', 'default', 'default'),    # job unscheduled
-            ('du', 'light blue', 'default', 'default'),    # delegated unscheduled
-            ('jb', 'light cyan', 'default', 'default'),    # job unscheduled with unfinished prereqs
-            ('by', 'dark magenta', 'default', 'default'),  # beginby"
-            ('ac', 'dark cyan', 'default', 'default'),     # action
-            ('ns', 'brown', 'default', 'default'),         # note scheduled
-            ('nu', 'brown', 'default', 'default'),         # note unscheduled
-            ('so', 'light blue', 'default', 'default'),    # someday
-            ('fn', 'dark gray', 'default', 'default'),     # finished task or job
-            ('df', 'brown', 'default', 'default'),     # default
-            ('dl', 'light gray', 'default', 'default'),    # deleted
-            ('co', 'light gray', 'default', 'default'),    # commented out
-            ]
-
-        self.screen = urwid.raw_display.Screen()
-        self.screen.set_mouse_tracking()
-        self.screen.tty_signal_keys('undefined', 'undefined', 'undefined', 'undefined', 'undefined')
-        self.screen.set_terminal_properties(256)
-        self.screen.register_palette(self.palette)
-
-
-        # Frame provices body, header and footer.
-        self.view = urwid.Frame(None, None, None)
-        self.set_header()
-        self.set_footer(text2="initializing")
-        placeholder = urwid.SolidFill()
-
-        self.loop = urwid.MainLoop(
-            placeholder,
-            screen=self.screen,
-            # unhandled_input=self.process_command,
-            unhandled_input=unhandled_input,
-        )
-
-        self.loop.widget = self.view
-        # self.loop.set_alarm_in(0.001, self.init_model)
-        self.today = None
         self.content = []
         self.refresh()
 
@@ -163,20 +131,19 @@ class BaseView:
         # else:
         #     # alrt = urwid.Text(urwid.Button("{}".format(alert)), align="center")
         #     alrt = urwid.Text(" 5:30pm+2 ", align="right")
-        self.view.footer = urwid.AttrMap(urwid.Columns([t1, ('fixed', len(text2)+1, t2)]), 'foot')
+        BaseView.view.footer = urwid.AttrMap(urwid.Columns([t1, ('fixed', len(text2)+1, t2)]), 'foot')
 
     def set_header(self, text1="event and task manager", text2="F1:help"):
         t1 = urwid.Text(" {}".format(text1), align="left")
         t2 = urwid.Text("{} ".format(text2), align='right')
-        self.saved_header = self.view.header
-        self.view.header = urwid.AttrMap(urwid.Columns([t1, ('fixed', len(text2)+1, t2)]), "title")
+        BaseView.view.header = urwid.AttrMap(urwid.Columns([t1, ('fixed', len(text2)+1, t2)]), "title")
 
     def set_body(self):
         """
 
         """
         # listbox will provide the body or main panel of the view (Frame)
-        self.view.body = urwid.AttrMap(urwid.ListBox(self.content),
+        BaseView.view.body = urwid.AttrMap(urwid.ListBox(self.content),
             'body', None)
 
     def add_centered(self, list_of_strings=[], style=None):
@@ -191,20 +158,33 @@ class BaseView:
         """
         self.content.extend([urwid.AttrMap(urwid.Text(x, wrap="space"), style, None) for x in list_of_strings])
 
-    def refresh(self, *args):
+    @classmethod
+    def refresh(cls, *args):
         now = pendulum.Pendulum.now()
         nxt = 60 - now.second
-        self.loop.set_alarm_in(nxt, self.refresh)
-        if now.date() != self.today:
-            self.today = now.date()
-            self.new_day()
-        self.set_footer(text1=now.format("h:mmA ddd MMM D", formatter='alternative'))
+        cls.loop.set_alarm_in(nxt, cls.refresh)
+        td = now.date()
+        if td != cls.today:
+            BaseView.today = td
+            cls.new_day()
+            yw = now.isocalendar()[:2]
+            if yw != cls.this_week:
+                cls.this_week = yw
+                cls.new_week()
+        cls.set_footer(cls, text1=now.format("h:mmA ddd MMM D", formatter='alternative'))
 
-    def new_day(self, today=None):
-        pass
+    @classmethod
+    def new_day(cls):
+        cls.set_footer("new day")
+
+    @classmethod
+    def new_week(cls):
+        cls.set_footer("new week")
+
 
 
 help_view = BaseView()
+help_view.set_header()
 help_view.add_centered(logo, 'logo')
 help_view.add_centered(menu, 'details')
 help_view.add_wrapped(menu_text, 'body')
