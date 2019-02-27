9ceeeed24529ed406452d67f47f9478448aa26f7
Dan Graham <dnlgrhm@gmail.com>
2019-02-27 18:34:12 -0500

    Added missing ruamel.yaml to requirements. Handle missing (None) expansions in etm properly and added missing 'quotes' to the tennis example in options

--- etmMVC/options.py
+++ etmMVC/options.py
@@ -62,7 +62,7 @@ alerts:
 # corresponding 'replacement string' values. E.g. with
 #
 #    expansions:
-#       tennis: @e 1h30m @a 30m: d @i personal:exercise 
+#       tennis: '@e 1h30m @a 30m: d @i personal:exercise' 
 #       ...
 #
 # then when '@x tennis' is entered the popup completions for 
