fb8fd6a4ef9b66c971a6a5f8d8a1c3c663d63ca8
Dan Graham <dnlgrhm@gmail.com>
2019-03-10 19:40:45 -0400

    Removed prep_dist.sh - the need functionality now provided by bump.py without the pattern, sed and grep nonsense.

--- bump.py
+++ bump.py
@@ -68,7 +68,7 @@ if new_version:
     check_output(f"git tag -a -f '{new_version}' -m '{version_info}'")
 
     count = 100
-    check_output(f"echo '# Recent changes as of {pendulum.now()}:' > CHANGES.txt")
+    check_output(f"echo 'Recent changes as of {pendulum.now()}:' > CHANGES.txt")
     check_output(f"git log --pretty=format:'- %ar%d %an%n    %h %ai%n%w(70,4,4)%B' --max-count={count} >> CHANGES.txt")
     check_output(f"git commit -a --amend -m '{tmsg}'")
 
