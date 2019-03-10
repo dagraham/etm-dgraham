fdddc667d490e7855d474718b1094d65a494c606
Dan Graham <dnlgrhm@gmail.com>
2019-03-10 19:19:25 -0400

    more bump tweaks

--- bump.py
+++ bump.py
@@ -69,7 +69,7 @@ if new_version:
     count = 100
     check_output(f"echo '# Recent changes as of {pendulum.now()}:' > CHANGES.txt")
     check_output(f"git log --pretty=format:'- %ar%d %an%n    %h %ai%n%w(70,4,4)%B' --max-count={count} >> CHANGES.txt")
-    check_output(f"git commit --amend -m '{tmsg} updated CHANGES.txt'")
+    check_output(f"git commit -a --amend -m '{tmsg} updated CHANGES.txt'")
 
 else:
     print(f"retained version: {version}")
